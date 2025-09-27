dad2bc3c98d4e959e609d69f2e854f8fc3b7eeae
budaizoli <bzoli.posta@gmail.com>
2025-09-27 16:11:26 +0200

    Delete download_images.sh

--- download_images.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-mkdir -p assets/from-web
-echo "Letöltés: Unsplash mintaképek..."
-curl -L "https://unsplash.com/photos/vCzXsrqGtk0/download?force=true" -o assets/from-web/electrician1.jpg
-curl -L "https://unsplash.com/photos/loAV8Aq0KdQ/download?force=true" -o assets/from-web/electrician2.jpg
-echo "Kész."
